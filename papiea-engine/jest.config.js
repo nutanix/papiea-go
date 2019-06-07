0738360ab01b2daf6fcd2e6017a9a31b7cd84627
Shlomi Vaknin <shlomivaknin@gmail.com>
2019-06-07 13:45:42 -0700

    Fixed testRegex

--- papiea-engine/jest.config.js
+++ papiea-engine/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '((\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node'
 }
