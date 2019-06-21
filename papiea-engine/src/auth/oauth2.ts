0bb15fa707bbfde1e7bf054acea1c3ed599b6ffa
Danylo Lytovchenko <evlen@qip.ru>
2019-06-21 15:27:25 +0300

    Use token from user info to revoke

--- papiea-engine/src/auth/oauth2.ts
+++ papiea-engine/src/auth/oauth2.ts
@@ -71,16 +71,19 @@ export function createOAuth2Router(redirect_uri: string, signature: Signature, p
 
     router.use('/provider/:prefix/:version/auth/logout', asyncHandler(async (req, res) => {
         const provider: Provider = await providerDb.get_provider(req.params.prefix, req.params.version);
-        const headers = {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-        }
-        console.log(req.headers)
-        const revokeResponse = await Axios.post(`${provider.oauth2.oauth.auth_host}${provider.oauth2.oauth.revoke_uri}`, querystring.stringify({"token": req.headers.authorization, "token_type_hint": "access_token"}), { headers, auth: {
-            username: provider.oauth2.oauth.client_id,
-            password: provider.oauth2.oauth.client_secret
-            }
-        });
-        console.log(revokeResponse.status);
+        // const headers = {
+        //     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+        // }
+        // console.log(req.headers)
+        // const revokeResponse = await Axios.post(`${provider.oauth2.oauth.auth_host}${provider.oauth2.oauth.revoke_uri}`, querystring.stringify({"token": req.headers.authorization, "token_type_hint": "access_token"}), { headers, auth: {
+        //     username: provider.oauth2.oauth.client_id,
+        //     password: provider.oauth2.oauth.client_secret
+        //     }
+        // });
+        // console.log(revokeResponse.status);
+        const oauth2 = getOAuth2(provider);
+        const token = oauth2.accessToken.create({ "access_token": req.user.authorization.split(' ')[1] });
+        token.revokeAll();
         return res.status(200).json("OK");
     })); 
 
