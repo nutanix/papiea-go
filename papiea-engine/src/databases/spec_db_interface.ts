418868968ace57d6f6dd0a333aa26623ed1bcc81
igorkorotach <igorkorotach@gmail.com>
2019-07-16 13:24:04 +0300

    Changed sort params to be optional

--- papiea-engine/src/databases/spec_db_interface.ts
+++ papiea-engine/src/databases/spec_db_interface.ts
@@ -29,7 +29,7 @@ export interface Spec_DB {
     // We could come up with command such as greater-than etc at some
     // later point, or we could use a similar dsl to mongodb search
     // dsl.
-    list_specs(fields_map: any, sortParams: SortParams): Promise<([Metadata, Spec])[]>;
+    list_specs(fields_map: any, sortParams?: SortParams): Promise<([Metadata, Spec])[]>;
 
     delete_spec(entity_ref: Entity_Reference): Promise<void>
 }
