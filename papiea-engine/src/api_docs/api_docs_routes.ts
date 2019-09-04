56acdbd0114c5a4477862a718f32095871bb08e3
Danylo Lytovchenko <evlen@qip.ru>
2019-09-04 17:57:19 +0300

    Remove * imports

--- papiea-engine/src/api_docs/api_docs_routes.ts
+++ papiea-engine/src/api_docs/api_docs_routes.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction, Router } from "express";
-import * as swaggerUi from "swagger-ui-express";
+import { setup, serve, generateHTML } from "swagger-ui-express";
 import ApiDocsGenerator from "./api_docs_generator";
 import { Provider_DB } from "../databases/provider_db_interface";
 import { Provider } from "papiea-core";
 import { readFileSync } from "fs";
-import * as pug from "pug";
+import { renderFile } from "pug";
 import { resolve } from "path";
 
 const admin_swagger = readFileSync(resolve(__dirname, 'admin_swagger.json'), 'utf8');
@@ -13,17 +13,17 @@ console.log(admin_swagger)
 async function swaggerSetupWrapper(req: Request, apiDocsGenerator: ApiDocsGenerator, providerDb: Provider_DB) {
     const provider: Provider = await providerDb.get_provider(req.params.provider, req.params.version);
     const apiDocJson: any = await apiDocsGenerator.getApiDocs(provider);
-    return swaggerUi.setup(apiDocJson);
+    return setup(apiDocJson);
 }
 
 export default function createAPIDocsRouter(urlPrefix: string, apiDocsGenerator: ApiDocsGenerator, providerDb: Provider_DB) {
     const apiDocsRouter = Router();
 
-    apiDocsRouter.use('/', swaggerUi.serve);
+    apiDocsRouter.use('/', serve);
 
     apiDocsRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
         const providers = await providerDb.list_providers();
-        res.send(pug.renderFile(resolve(__dirname, 'main_swagger.pug'), {providers: providers}));
+        res.send(renderFile(resolve(__dirname, 'main_swagger.pug'), {providers: providers}));
     });
 
     apiDocsRouter.get('*/swagger-ui-init.js', async (req: Request, res: Response, next: NextFunction) => {
@@ -47,7 +47,7 @@ export default function createAPIDocsRouter(urlPrefix: string, apiDocsGenerator:
     });
 
     apiDocsRouter.get('/admin', async (req: Request, res: Response, next: NextFunction) => {
-        res.send(swaggerUi.generateHTML(JSON.parse(admin_swagger)));
+        res.send(generateHTML(JSON.parse(admin_swagger)));
     });
 
     return apiDocsRouter;
