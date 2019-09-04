a8841494f88096399529c9303d0d9626b79b3da1
Aleksandr Dolgaryev <adolgarev@gmail.com>
2019-09-04 18:26:43 +0300

    Check that there are no excess params

--- papiea-engine/src/entity/entity_routes.ts
+++ papiea-engine/src/entity/entity_routes.ts
@@ -3,7 +3,12 @@ import { Router } from "express";
 import { UserAuthInfo, asyncHandler } from '../auth/authn';
 import { processPaginationParams, processSortQuery } from "../utils/utils";
 import { SortParams } from "./entity_api_impl";
-import { BadRequestError } from "../errors/bad_request_error";
+import { CheckNoQueryParams, check_request } from "../validator/express_validator";
+
+const CheckProcedureCallParams = check_request({
+    allowed_query_params: [],
+    allowed_body_params: ['input']
+});
 
 export function createEntityAPIRouter(entity_api: Entity_API): Router {
     const router = Router();
@@ -31,29 +36,19 @@ export function createEntityAPIRouter(entity_api: Entity_API): Router {
         return { results: pageEntities, entity_count: totalEntities };
     };
 
-    router.post("/:prefix/:version/check_permission", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/check_permission", CheckNoQueryParams, asyncHandler(async (req, res) => {
         res.json(await entity_api.check_permission(req.user, req.params.prefix, req.params.version, req.body))
     }));
 
-    router.get("/:prefix/:version/:kind", asyncHandler(async (req, res) => {
+    router.get("/:prefix/:version/:kind", check_request({
+        allowed_query_params: ['offset', 'limit', 'sort', 'spec', 'status', 'metadata']
+    }), asyncHandler(async (req, res) => {
         const filter: any = {};
         const offset: undefined | number = req.query.offset;
         const limit: undefined | number = req.query.limit;
         const rawSortQuery: undefined | string = req.query.sort;
         const sortParams = processSortQuery(rawSortQuery);
         const [skip, size] = processPaginationParams(offset, limit);
-        const allowed_query_params = new Set()
-            .add('offset')
-            .add('limit')
-            .add('sort')
-            .add('spec')
-            .add('status')
-            .add('metadata');
-        for (var req_query_param in req.query) {
-            if (!allowed_query_params.has(req_query_param)) {
-                throw new BadRequestError(`Allowed query params ${Object.keys(allowed_query_params)}`);
-            }
-        }
         if (req.query.spec) {
             filter.spec = JSON.parse(req.query.spec);
         } else {
@@ -72,37 +67,22 @@ export function createEntityAPIRouter(entity_api: Entity_API): Router {
         res.json(await filterEntities(req.user, req.params.kind, filter, skip, size, sortParams));
     }));
 
-    router.get("/:prefix/:version/:kind/:uuid", asyncHandler(async (req, res) => {
+    router.get("/:prefix/:version/:kind/:uuid", CheckNoQueryParams, asyncHandler(async (req, res) => {
         const [metadata, spec] = await entity_api.get_entity_spec(req.user, req.params.kind, req.params.uuid);
         const [_, status] = await entity_api.get_entity_status(req.user, req.params.kind, req.params.uuid);
         res.json({ "metadata": metadata, "spec": spec, "status": status });
     }));
 
-    router.post("/:prefix/:version/:kind/filter", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/:kind/filter", check_request({
+        allowed_query_params: ['offset', 'limit', 'sort'],
+        allowed_body_params: ['spec', 'status', 'metadata']
+    }), asyncHandler(async (req, res) => {
         const offset: undefined | number = req.query.offset;
         const limit: undefined | number = req.query.limit;
         const rawSortQuery: undefined | string = req.query.sort;
         const sortParams: undefined | SortParams = processSortQuery(rawSortQuery);
         const [skip, size] = processPaginationParams(offset, limit);
         const filter: any = {};
-        const allowed_query_params = new Set()
-            .add('offset')
-            .add('limit')
-            .add('sort');
-        for (var req_query_param in req.query) {
-            if (!allowed_query_params.has(req_query_param)) {
-                throw new BadRequestError(`Allowed query params ${Object.keys(allowed_query_params)}`);
-            }
-        }
-        const allowed_body_params = new Set()
-            .add('spec')
-            .add('status')
-            .add('metadata');
-        for (var req_body_param in req.body) {
-            if (!allowed_body_params.has(req_body_param)) {
-                throw new BadRequestError(`Allowed body params ${Object.keys(allowed_body_params)}`);
-            }
-        }
         if (req.body.spec) {
             filter.spec = req.body.spec;
         } else {
@@ -122,13 +102,19 @@ export function createEntityAPIRouter(entity_api: Entity_API): Router {
         res.json(await filterEntities(req.user, req.params.kind, filter, skip, size, sortParams));
     }));
 
-    router.put("/:prefix/:version/:kind/:uuid", asyncHandler(async (req, res) => {
+    router.put("/:prefix/:version/:kind/:uuid", check_request({
+        allowed_query_params: [],
+        allowed_body_params: ['metadata', 'spec']
+    }), asyncHandler(async (req, res) => {
         const request_metadata = req.body.metadata;
         const [metadata, spec] = await entity_api.update_entity_spec(req.user, req.params.uuid, req.params.prefix, request_metadata.spec_version, request_metadata.extension, req.params.kind, req.params.version, req.body.spec);
         res.json({ "metadata": metadata, "spec": spec });
     }));
 
-    router.post("/:prefix/:version/:kind", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/:kind", check_request({
+        allowed_query_params: [],
+        allowed_body_params: ['metadata', 'spec']
+    }), asyncHandler(async (req, res) => {
         if (req.params.status) {
             const [metadata, spec] = await entity_api.save_entity(req.user, req.params.prefix, req.params.kind, req.params.version, req.body.spec, req.body.metadata);
             res.json({ "metadata": metadata, "spec": spec });
@@ -139,22 +125,22 @@ export function createEntityAPIRouter(entity_api: Entity_API): Router {
         }
     }));
 
-    router.delete("/:prefix/:version/:kind/:uuid", asyncHandler(async (req, res) => {
+    router.delete("/:prefix/:version/:kind/:uuid", CheckNoQueryParams, asyncHandler(async (req, res) => {
         await entity_api.delete_entity_spec(req.user, req.params.prefix, req.params.version, req.params.kind, req.params.uuid);
         res.json("OK")
     }));
 
-    router.post("/:prefix/:version/:kind/:uuid/procedure/:procedure_name", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/:kind/:uuid/procedure/:procedure_name", CheckProcedureCallParams, asyncHandler(async (req, res) => {
         const result: any = await entity_api.call_procedure(req.user, req.params.prefix, req.params.kind, req.params.version, req.params.uuid, req.params.procedure_name, req.body.input);
         res.json(result);
     }));
 
-    router.post("/:prefix/:version/:kind/procedure/:procedure_name", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/:kind/procedure/:procedure_name", CheckProcedureCallParams, asyncHandler(async (req, res) => {
         const result: any = await entity_api.call_kind_procedure(req.user, req.params.prefix, req.params.kind, req.params.version, req.params.procedure_name, req.body.input);
         res.json(result);
     }));
 
-    router.post("/:prefix/:version/procedure/:procedure_name", asyncHandler(async (req, res) => {
+    router.post("/:prefix/:version/procedure/:procedure_name", CheckProcedureCallParams, asyncHandler(async (req, res) => {
         const result: any = await entity_api.call_provider_procedure(req.user, req.params.prefix, req.params.version, req.params.procedure_name, req.body.input);
         res.json(result);
     }));
